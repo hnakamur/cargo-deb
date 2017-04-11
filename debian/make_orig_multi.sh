46be57f1ea082e62dd14f9928f9b98a934aba854
Ximin Luo <infinity0@debian.org>
2017-04-11 19:58:50 +0200

    Update Debian config and scripts for cargo 0.16

--- debian/make_orig_multi.sh
+++ debian/make_orig_multi.sh
@@ -45,7 +45,7 @@ export GIT_AUTHOR_NAME="deb-build"
 export GIT_AUTHOR_EMAIL="<>"
 export GIT_COMMITTER_NAME="${GIT_AUTHOR_NAME}"
 export GIT_COMMITTER_EMAIL="${GIT_AUTHOR_EMAIL}"
-cargo vendor --verbose
+cargo vendor --verbose deps
 
 # Unpack artifacts and clean embedded libs
 ${WORKDIR}/debian/cargo-vendor-unpack.py
